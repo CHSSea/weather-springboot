e62f18858303dbc300d062e52693f49d7852b7ad
haisheng <775459833@qq.com>
2019-04-04 09:13:38 +0800

    添加Redis配置

---
+++ src/main/java/com/example/springboot/bean/User.java
@@ -0,0 +1,51 @@
+package com.example.springboot.bean;
+
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+
+import java.io.Serializable;
+
+/**
+ * @program: springboot
+ * @description: 用户类
+ * @author: Haisheng
+ * @create: 2019-04-02 17:14
+ **/
+public class User implements Serializable {
+    private int id;
+    private String userName;
+    private String password;
+    private String redisKey;
+
+    public String getRedisKey() {
+        return redisKey;
+    }
+
+    public void setRedisKey(String redisKey) {
+        this.redisKey = redisKey;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/example/springboot/controller/RedisTestController.java
@@ -0,0 +1,47 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.bean.User;
+import com.example.springboot.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @program: springboot
+ * @description: test Redis class
+ * @author: haisheng
+ * @create: 2019-04-02 16:55
+ **/
+
+@RestController
+@RequestMapping("/redis")
+public class RedisTestController {
+
+    @Autowired
+    public RedisUtil redisUtil;
+
+    @RequestMapping(value = "/set")
+    public String set(){
+        String username = "mumu";
+        String password = "111";
+        boolean result = redisUtil.set(username,password);
+        if(result){
+            return "SUCCESS";
+        }else{
+            return "FALSE";
+        }
+    }
+
+    @GetMapping("/get/{key}")
+    public Object get(@PathVariable(name = "key") String key){
+        return redisUtil.get(key);
+    }
+
+    @PostMapping(value = "find")
+    @ResponseBody
+    public Object find(String key){
+        return redisUtil.get(key);
+    }
+
+}
