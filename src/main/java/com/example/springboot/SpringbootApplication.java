29cf5bceff9fffc88c587c227e378b9652e70c50
haisheng <775459833@qq.com>
2019-04-17 15:29:10 +0800

    删除jersey的配置

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,14 +2,19 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(SpringbootApplication.class);
+    }
 }
